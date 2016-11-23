bdb7d57539c47c4cf23a6791119f4ef45299a409
wuben <benwu0506@gmail.com>
2016-11-22 23:08:35 -0500

    Started nodes and branches for eventual A*.

--- Floodit AI.py
+++ Floodit AI.py
@@ -17,7 +17,7 @@ while True:
 def setup_Board():
     x = int(input("How many columns? "))
     y = int(input("How many rows? "))
-    variables = int(input("How many numbers to fill? "))
+    variables = 6
     
     board = [[random.randint(0,variables) for j in range(0,x)] for i in range(0,y)]
     return board
@@ -30,15 +30,15 @@ def print_Board(board):
 #resulting from it.
 def make_Move(move,board):
     newBoard = deepcopy(board)
-    test = flood_Fill(newBoard,0,0,move,board[0][0])
+    newBoard = flood_Fill(newBoard,0,0,move,board[0][0])
     #return newBoard
-    return test
+    return newBoard
 
 
 def flood_Fill(board,row,col,move,curVal):
-    
+    #fills board with move returns None if move is the same as current home    
     if move == curVal or board[row][col] != curVal:
-        return 0
+        return None
     board[row][col] = move
     
     if row> 0:
@@ -53,13 +53,6 @@ def flood_Fill(board,row,col,move,curVal):
     
     return board
 
-
-#Returns how many blocks are flooded
-def quantify_Flood(board):
-    count = 0
-    floodVal = board[0][0]
-    return 0
-
 #Sees if board is flooded 
 def victory_Bool(board):
     for x in range(len(board[0])):
@@ -67,4 +60,59 @@ def victory_Bool(board):
             if board[x][y] != board[0][0]:
                 return False
     return True
+
+
+#=============================
+#NOT DONE
+#=============================
+#Returns how many blocks are flooded
+def quantify_Flood(board):
+    count = 0
+    floodVal = board[0][0]
+    return 0
+
+
+
+
+#Node will hold floodit state to be used in the search functions
+class Node:
+    def __init__(self,board,parent,move,depth,cost):
+        self.board = board
+        self.parent = parent
+        self.move = move
+        self.depth = depth
+        self.cost = cost
+        self.f = depth + cost
+
+
+def flood_Solver(board,variables,limit):
+    base = Node(board,None,None,0,0)
+
+def expand_Moves(node,variables):
+    #returns list of all boards(nodes) made with each variable
+    branches = []
+
+    for i in range(variables):
+        branches.append(Node(make_Move(i,node.board),node,i,node.depth+1,0))
+    branches = [node for node in branches if node.board != None]
+    
+    return branches
+
+def main():
+    home = setup_Board()
+    variables = 6
+
+    print_Board(home)
+    print("Ready for move")
+
+    homeNode = Node(home,None,None,0,0)
+    moveSet = expand_Moves(homeNode,6)
+
+    print "Branches"
+    for move in moveSet:
+        print_Board(move.board)
+        print ""
+
+
+
     
